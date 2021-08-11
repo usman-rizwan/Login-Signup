6b963d1130447cf8128c32ef92cd9dfd011336d2
usman_rizwan <81845304+usman-rizwan@users.noreply.github.com>
2021-08-11 22:32:58 +0500

    Add files via upload

---
+++ login 2/app.js
@@ -0,0 +1,72 @@
+// Show Password
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+// console.log(firebase)
+
+
+
+let register = () => {
+  let username = document.getElementById("username");
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let loader = document.getElementById("loader");
+  let signupText = document.getElementById("loaderText");
+  loaderText.style.display = "none";
+  loader.style.display = "block";
+
+  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    .then((res) => {
+      firebase.database().ref(`users/${res.user.uid}`).set({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+      })
+        .then(() => {
+          let successDiv = document.getElementById("successDiv");
+          successDiv.innerHTML = "Uer register successfully"
+          successDiv.style.display = "block";
+          errorDiv.style.display = "none";
+          username.value = ""
+          email.value = ""
+          password.value = ""
+          loaderText.style.display = "block";
+          loader.style.display = "none ";
+          setTimeout(
+            () => {
+              window.location = "login.html"
+            }, 1500)
+        })
+
+    })
+    .catch((err) => {
+      let errorDiv = document.getElementById("errorDiv");
+      errorDiv.innerHTML = err.message;
+      errorDiv.style.display = "block"
+      loaderText.style.display = "block";
+      loader.style.display = "none";
+
+    });
+
+}
+
+let login = ()=>{
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .then((res) => {
+    // Signed in
+    console.log("USer sddd",user.res)
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
